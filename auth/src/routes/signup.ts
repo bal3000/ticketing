1be7ddc436dfb8a791b7ea870c35082c2f85c391
bal3000 <bal3000286@googlemail.com>
2020-07-07 19:18:28 +0100

    error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!email || typeof email !== 'string') {
-    res.send(400).send('Provide a valid email');
+    if (!errors.isEmpty()) {
+      throw new Error('Invalid email or password');
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating a user...');
+    throw new Error('Error connecting to database');
+
+    res.send({});
   }
-});
+);
 
 export { router as signupRouter };
