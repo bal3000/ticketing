a5654c657228c3c4304598de5f06bc52d3abd1e4
bal3000 <bal3000286@googlemail.com>
2020-07-21 16:41:55 +0100

    initail commit

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import buildClient from '../api/build-client';
+// import buildClient from '../api/build-client';
 
 function LandingPage({ currentUser }) {
-  return <h1>{currentUser ? 'You are signed in' : 'You are signed out'}</h1>;
+  return <h1>{currentUser ? 'You are signed in!' : 'You are signed out!'}</h1>;
 }
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
-};
+// LandingPage.getInitialProps = async (context) => {
+//   const client = buildClient(context);
+//   const { data } = await client.get('/api/users/currentuser');
+//   return data;
+// };
 
 export default LandingPage;
