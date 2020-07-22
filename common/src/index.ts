040edfb7b9396ddf26ea21dd0c9f5f70b1c09c61
bal3000 <bal3000286@googlemail.com>
2020-07-22 08:33:10 +0100

    moved to common

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,12 @@
-export interface Demo {
-  prop1: string;
-  prop2: number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/error-message.interface';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
