7f8a2987683741a41083bb0f3cb95c18676e721d
bal3000 <bal3000286@googlemail.com>
2020-07-24 17:02:15 +0100

    error handling

--- tickets/src/routes/show-ticket.ts
+++ tickets/src/routes/show-ticket.ts
@@ -1,24 +1,18 @@
 import express, { Request, Response } from 'express';
-import { param } from 'express-validator';
-import { NotFoundError, validateRequest } from '@tripb3000/common';
+import { NotFoundError } from '@tripb3000/common';
 
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get(
-  '/api/tickets/:id',
-  [param('id').isInt({ gt: 0 }).withMessage('Id is required')],
-  validateRequest,
-  async (req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
 
-    if (!ticket) {
-      return new NotFoundError();
-    }
-
-    res.send(ticket);
+  if (!ticket) {
+    return new NotFoundError();
   }
-);
+
+  res.send(ticket);
+});
 
 export { router as showTicketRouter };
