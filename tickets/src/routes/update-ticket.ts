5c4052fb5c219fae9f8aaef2bdb76f80f2a09fe5
bal3000 <bal3000286@googlemail.com>
2020-07-28 16:08:07 +0100

    update ticket

--- tickets/src/routes/update-ticket.ts
+++ tickets/src/routes/update-ticket.ts
@@ -8,6 +8,8 @@ import {
 } from '@tripb3000/common';
 
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -39,6 +41,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.sendStatus(204);
   }
