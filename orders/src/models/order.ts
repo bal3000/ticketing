314a02f4e09fe74f478739e4aea2ac0bfbfbd2bf
bal3000 <bal3000286@googlemail.com>
2020-07-30 17:35:04 +0100

    mongoose - order

---
+++ orders/src/models/order.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDocument;
+}
+
+interface OrderDocument extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDocument;
+}
+
+interface OrderModel extends mongoose.Model<OrderDocument> {
+  build(attrs: OrderAttrs): OrderDocument;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDocument, OrderModel>('Order', orderSchema);
+
+export { Order };
