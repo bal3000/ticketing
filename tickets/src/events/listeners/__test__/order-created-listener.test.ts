0426cfc6b89a06ace52f6e83da60149368531b96
bal3000 <bal3000286@googlemail.com>
2020-08-06 12:53:25 +0100

    test the publish

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -58,3 +58,17 @@ it('acks the message', async () => {
   // write assertions to make sure a ack function was created
   expect(message.ack).toHaveBeenCalled();
 });
+
+it('publishes a ticket updated event', async () => {
+  const { listener, data, message } = await setup();
+
+  await listener.onMessage(data, message);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(data.id).toEqual(ticketUpdatedData._doc.orderId);
+});
