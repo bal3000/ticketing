f34762cb2b64b80d83d1812c2051eb6acfd5f3c1
bal3000 <bal3000286@googlemail.com>
2020-08-07 17:55:39 +0100

    listener and queue created

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, OrderCreatedEvent } from '@tripb3000/common';
 
 import { queueGroupName } from './constants';
+import expirationQueue from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -11,6 +12,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     data: OrderCreatedEvent['data'],
     msg: Message
   ): Promise<void> {
+    await expirationQueue.add({ orderId: data.id });
     // Ack the message
     msg.ack();
   }
