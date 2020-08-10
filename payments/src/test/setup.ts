29b59df464fc4be63f305e856e16b67c9f3b1a54
bal3000 <bal3000286@googlemail.com>
2020-08-10 17:15:24 +0100

    payments setup

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,59 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
+import app from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+let mongo: any;
+
+jest.mock('../nats-wrapper');
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'test';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  collections.forEach(async (collection) => {
+    await collection.deleteMany({});
+  });
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Create JWT
+  const token = jwt.sign(
+    {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: 'test@test.com',
+    },
+    process.env.JWT_KEY!
+  );
+
+  // Take JSON and encode it to base64
+  const base64 = Buffer.from(JSON.stringify({ jwt: token })).toString('base64');
+
+  // return a string thats the cookie
+  return [`express:sess=${base64}`];
+};
